460e0d09918036a1b4f9dc84840b8ba398ef12e7
MattCrl <mattblogdev@gmail.com>
2025-08-27 18:35:39 +0200

    2015 day 2 part 2 done

--- 2015/day02/solution_spec.rb
+++ 2015/day02/solution_spec.rb
@@ -10,5 +10,7 @@ RSpec.describe 'Advent of Code Day 2' do
   end
 
   it 'runs examples for part 2' do
+    expect(WrappingPaperCalculator.new('2x3x4', 'x').square_feet_of_ribon).to eq(34)
+    expect(WrappingPaperCalculator.new('1x1x10', 'x').square_feet_of_ribon).to eq(14)
   end
 end
--- 2015/day02/wrapping_paper_calculator.rb
+++ 2015/day02/wrapping_paper_calculator.rb
@@ -17,10 +17,20 @@ class WrappingPaperCalculator
     total_paper_needed
   end
 
+  def square_feet_of_ribon
+    total_ribon_needed = 0
+    dimensions.split("\n").map do |one_gift_size|
+      l, w, h = one_gift_size.split('x').map &:to_i
+      smallest_size_perimeter = [(l * 2) + (w * 2), (w * 2) + (h * 2), (h * 2) + (l * 2)].min
+      total_ribon_needed += smallest_size_perimeter + (l * w * h)
+    end
+    total_ribon_needed
+  end
+
   private
 
   def one_gift_paper_size(length, width, height)
-    smallest_side = [length * width, width * height, height * length].min
-    (2 * length * width) + (2 * width * height) + (2 * height * length) + smallest_side
+    smallest_side_area = [length * width, width * height, height * length].min
+    (2 * length * width) + (2 * width * height) + (2 * height * length) + smallest_side_area
   end
 end
