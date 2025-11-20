d2cb3bbfc7d4586757d27e779ea66f8bb7729434
MattCrl <mattblogdev@gmail.com>
2025-11-20 08:01:20 +0100

    2015 day 8 part 2 done

--- 2015/day08/solution.rb
+++ 2015/day08/solution.rb
@@ -3,5 +3,8 @@ require_relative 'string_calculator'
 input_path = File.join(__dir__, 'input.txt')
 lines = File.read(input_path).strip.split("\n")
 
-inputs = StringCalculator.new(lines)
-inputs.calculate_escaped_chars
+inputs_p1 = StringCalculator.new(lines)
+inputs_p1.calculate_escaped_chars
+
+inputs_p2 = StringCalculator.new(lines)
+inputs_p2.calculate_escaped_chars_p2
--- 2015/day08/string_calculator.rb
+++ 2015/day08/string_calculator.rb
@@ -15,10 +15,24 @@ class StringCalculator
 
       line = replace_escaped_chars(line)
       
-      total_escaped += line.size - 2
+      total_escaped += line.size - 2 # -2 for the double quotes at the start/end of every line
     end
 
-    puts total_non_escaped - total_escaped
+    puts "part 1 = #{total_non_escaped - total_escaped}"
+  end
+
+  def calculate_escaped_chars_p2
+    total_non_escaped = 0
+    total_escaped = 0
+    lines.each do |line|
+      total_non_escaped += line.size
+
+      line = replace_escaped_chars_p2(line)
+      
+      total_escaped += line.size + 4 # +4 for the double quotes at the start/end of every line
+    end
+
+    puts "part 2 = #{total_escaped - total_non_escaped }"
   end
 
   private
@@ -27,6 +41,13 @@ class StringCalculator
     line = line.gsub(/\\x[0-9a-fA-F]{2}/, ".")
     line = line.gsub(/\\\\/, ".")
     line = line.gsub(/\\\"/, ".")
+    line
+  end
+
+  def replace_escaped_chars_p2(line)
+    line = line.gsub(/\\x[0-9a-fA-F]{2}/, ".....")
+    line = line.gsub(/\\\\/, "....")
+    line = line.gsub(/\\\"/, "....")
     line
   end
 end
