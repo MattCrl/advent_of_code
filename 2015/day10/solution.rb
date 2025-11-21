6e2669fd3f5a53562575328ee18bbf9f7bd16858
MattCrl <mattblogdev@gmail.com>
2025-11-21 08:37:55 +0100

    2015 day 10 part 2 done

--- 2015/day10/solution.rb
+++ 2015/day10/solution.rb
@@ -2,7 +2,14 @@
 
 require_relative 'look_and_say'
 
+# p1
 look_and_say = LookAndSay.new("1113222113")
 look_and_say.play_game(40)
 
-puts look_and_say.input
+puts look_and_say.input.length
+
+# p2
+look_and_say = LookAndSay.new("1113222113")
+look_and_say.play_game(50)
+
+puts look_and_say.input.length
