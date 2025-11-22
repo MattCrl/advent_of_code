3cfb871c4e7e505d172ac562dd1d97babf12a687
MattCrl <mattblogdev@gmail.com>
2025-11-22 17:55:53 +0100

    2015 day 11 part 2 done

--- 2015/day11/password.rb
+++ 2015/day11/password.rb
@@ -1,20 +1,21 @@
 # frozen_string_literal: true
 
 class Password
-  attr_reader :forbidden_letters, :forbidden_words, :password, :valid_password
+  attr_reader :forbidden_letters, :forbidden_words, :password
 
   def initialize(forbidden_letters, forbidden_words, password)
     @forbidden_letters = forbidden_letters
     @forbidden_words = forbidden_words
     @password = password
-    @valid_password = false
   end
 
   def find_next_valid_password
-    while !@valid_password
+    password_is_invalid = true
+
+    while password_is_invalid
       @password = @password.next
       if @forbidden_words.any? { |word| @password.include?(word) } && !(@forbidden_letters.any? { |word| @password.include?(word) }) && (@password.scan(/(.)\1/).uniq.count > 1)
-        @valid_password = true
+        password_is_invalid = false
       end
     end
 
--- 2015/day11/solution.rb
+++ 2015/day11/solution.rb
@@ -7,4 +7,5 @@ forbidden_words = "abcdefghijklmnopqrstuvwxyz".chars.each_cons(3).map(&:join)
 
 password = Password.new(forbidden_letters, forbidden_words, "hxbxwxba")
 
-puts password.find_next_valid_password
+puts password.find_next_valid_password # p1
+puts password.find_next_valid_password # p2
